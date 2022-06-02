1277db4221353993da3380299d16345c2156cf6d
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2022-06-02 20:55:14 +0000

    hold me

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def home():
 def donate():
     return render_template('donate.html')
 
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
 @app.route('/addscripture')
 def addscripture():
     pass
